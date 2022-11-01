ce32c93c6ac09f958a904da471936d094919cb87
Raghav1999 <raghavsharma199923@gmail.com>
2022-11-01 12:42:37 +0530

    Empty String/ Result Not Found Bug Fixes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        setMovies(data.Search);
+        if (data.Response !== 'False')
+            setMovies(data.Search);
+        else {
+            setMovies([]);
+        }
     }
 
     useEffect(() => {
